a7916f539aee46c48508c2b05a39b33568b2c196
Rees McDevitt <mcdevittrees@gmail.com>
2024-09-26 14:50:27 -0400

    adding router basics

---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./NavBar";
+
+function About() {
+  return (
+    <div>
+      <Navbar />
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./NavBar";
+
+function Contact() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Contact</h1>
+    </div>
+  );
+}
+
+export default Contact;
